1fe3401872b82fab390af735e1dce7946f007ac1
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-10-03 10:55:11 +0000

    Fri Oct 03 10:55:11 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,8 @@
 
 class SumSolution:
     
-    def compute(self, x, y):
+    def compute(self, x: int, y: int):
+        if 0 <= x <= 100 or 0 <= y <= 100:
+            raise ValueError("Invalid argument, must be integer between 0 and 100")
         raise NotImplementedError()
+
