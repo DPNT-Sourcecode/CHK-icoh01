3b49ccfb46a8697923847d91ec16dee0af3bdfde
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-10-03 11:24:25 +0000

    Fri Oct 03 11:24:25 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,13 +1,14 @@
-from pydantic import TypeAdapter, constr
+from pydantic import TypeAdapter, constr, ConfigDict
 
 Name = constr()
 
-adapter = TypeAdapter(str)
+adapter = TypeAdapter(str, config=ConfigDict(strict=True))
 
 class HelloSolution:
     def hello(self, friend_name: str) -> str:
         adapter.validate_python(friend_name)
         return f'Hello, {friend_name}!'
+
 
 
 
