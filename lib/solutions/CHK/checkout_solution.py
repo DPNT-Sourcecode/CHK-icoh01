46e277f5670f5fa694fe41982108a56e468a6e2f
sbx_user1051 <sbx_user1051@169.254.16.143>
2025-10-03 12:16:31 +0000

    Fri Oct 03 12:16:31 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 products = {
     "A": 50,
     "B": 30,
@@ -5,11 +7,18 @@ products = {
     "D": 15,
 }
 
+offers = {
+    "A": {"count": 3, "price": 130},
+    "B": {"count": 2, "price": 45},
+}
+
 class CheckoutSolution:
     def checkout(self, skus: str):
         if not isinstance(skus, str):
             raise ValueError("Invalid input, SKUs must be a string")
         raw_skus = [sku for sku in skus]
+        counts = Counter(raw_skus)
+
 
 
 
