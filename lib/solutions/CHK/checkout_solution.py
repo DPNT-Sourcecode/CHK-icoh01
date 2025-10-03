93c58e6cd1c0999b367afbcbc2f52918dff615a4
sbx_user1051 <sbx_user1051@169.254.16.143>
2025-10-03 12:13:31 +0000

    Fri Oct 03 12:13:31 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,16 @@
+products = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
 class CheckoutSolution:
     def checkout(self, skus: str):
-        raise NotImplementedError()
+        if not isinstance(skus, str):
+            raise ValueError("Invalid input, SKUs must be a string")
+        raw_skus = [sku for sku in skus]
+
 
 
 
--- test/solution_tests/CHK/R1/test_checkout.py
+++ test/solution_tests/CHK/R1/test_checkout.py
@@ -11,16 +11,13 @@ class MockIntType:
 
 
 @pytest.mark.parametrize(
-    "addend1,addend2,expected",
+    "skus,expected",
     [
-        (1, 2, 3),
-        (0, 0, 0),
-        (100, 100, 200),  # assuming 0-100 is inclusive
-        (35, 66, 101),
-        (55, 92, 147),
+        ("ABCD", 115)
     ],
 )
 def test_valid_inputs(addend1, addend2, expected):
     assert CheckoutSolution().checkout(addend1, addend2) == expected
+
 
 
