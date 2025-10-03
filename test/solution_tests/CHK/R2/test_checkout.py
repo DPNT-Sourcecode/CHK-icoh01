87cf71c5800333883fe6b4068e2c6b725c43a15b
sbx_user1051 <sbx_user1051@169.254.16.143>
2025-10-03 12:56:48 +0000

    Fri Oct 03 12:56:48 UTC 2025

--- test/solution_tests/CHK/R2/test_checkout.py
+++ test/solution_tests/CHK/R2/test_checkout.py
@@ -7,7 +7,16 @@ from solutions.CHK.checkout_solution import CheckoutSolution
     "skus,expected",
     [
         ("AAA", 130),
+        ("AAAAA", 200),
+        ("AAAAAA", 250),
+        ("AAAAAAAA", 330),
+        ("AAAAAAAAAA", 400),
         ("BB", 45),
+        ("EE", 80),
+        ("EEB", 80),
+        ("EEBB", 110),
+        ("EEBBB", 125),
+        ("EAEBCBBAA", 125+130+20),
         ("AAABB", 175),
         ("ABCDABCD", 215),
         ("ABCD", 115),
@@ -29,4 +38,3 @@ def test_valid_inputs(skus, expected):
 )
 def test_invalid_inputs(skus):
     assert CheckoutSolution().checkout(skus) == -1
-
